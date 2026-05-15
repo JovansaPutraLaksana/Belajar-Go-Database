fe8ab5177140c211ae60ceac2a607008fecb2afa
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-05-15 16:03:12 +0700

    connect ke database

--- database_test.go
+++ database_test.go
@@ -1,6 +1,7 @@
 package belajar_go_database
 
 import (
+	"database/sql"
 	"fmt"
 	"testing"
 
@@ -9,4 +10,13 @@ import (
 
 func TestEmpty(t *testing.T) {
 	fmt.Println("Test Empty")
+}
+
+// koneksi sql ini bersifat rahasia (saya tidak menggunakan localhost karena laptop saya tidak terinstall mysql), namun ini adalah sql dummy yang bisa digunakan untuk belajar, jadi tidak masalah jika dibagikan
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "u7jrrwm6d2do347l:jD4C66nOGnoFxyr3Vxws@tcp(blzvlgzphf7oinbjckkg-mysql.services.clever-cloud.com:3306)/belajar-go-database")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
